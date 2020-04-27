f4569855b1a3c79060305f3573047918617a7203
pearson <chenh348@mail2.sysu.edu.cn>
2020-04-27 11:26:10 +0800

    init: factor price fixed point problem

--- Full_Model/Baseline/src/Full_Baseline_Functions.jl
+++ Full_Model/Baseline/src/Full_Baseline_Functions.jl
@@ -2,3 +2,12 @@
 ## Date: April 2020
 ## Julia version: 1.3.1
 ## Purpose: Functions for EKNR(2016) Full Model - Baseline Equilibrium
+
+function p_fixpoint(
+    res_fixpoint::AbstractArray,
+    guess_fixpoint::AbstractArray,
+    exos_fixpoint::NamedTuple,
+    params_fixpoint::NamedTuple,
+    )
+
+end
